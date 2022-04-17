bff2f5767816eae2927a2809a98f836ec4868329
Joe Wilson <wilsonmangunsong@gmail.com>
2022-04-17 21:58:51 +0700

    employeeEntity
    employee entity

--- project-management/src/main/java/com/wilson/pma/entities/Employee.java
+++ project-management/src/main/java/com/wilson/pma/entities/Employee.java
@@ -1,3 +1,18 @@
+package com.wilson.pma.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long employeeId;
+	
+	private String firstName;
 	private String lastName;
 	private String email;
 	
