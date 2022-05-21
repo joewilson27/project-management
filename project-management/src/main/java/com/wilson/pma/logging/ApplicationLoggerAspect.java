d9d25535853a3031abaef93ae902c338ff7ff156
Joe Wilson <wilsonmangunsong@gmail.com>
2022-05-22 01:21:13 +0700

    sec11_vid50
    Section vid50
    A Joint Point is basically a point during the execution of your program
    Join Point object gives us the ability to access points during the execution of your program. Jadi intinya, join point akan memberikan akses kepada kita utk mengakses apapun yg ada dalam method / fungsi yg sedang berjalan, misalnya argumen, variable dll yg ada dalam proses tersebut. Sehingga kita dapat membuat log dari sana
    
    Sample log with advice before
    2022-05-22 00:47:27.756[0;39m [32mDEBUG[0;39m [35m13072[0;39m [2m---[0;39m [2m[nio-8080-exec-2][0;39m [36mc.w.pma.logging.ApplicationLoggerAspect [0;39m [2m:[0;39m ************* Before Method Execution ***************
     com.wilson.pma.controllers.HomeController...displayHome () with arguments[s] = [{}]
    [2m2022-05-22 00:47:27.757[0;39m [32mDEBUG[0;39m [35m13072[0;39m [2m---[0;39m [2m[nio-8080-exec-2][0;39m [36mc.w.pma.logging.ApplicationLoggerAspect [0;39m [2m:[0;39m _________________________________________________
    
    Advice @Around adalah advice gabungan dari @After dan @Before advice

--- project-management/src/main/java/com/wilson/pma/logging/ApplicationLoggerAspect.java
+++ project-management/src/main/java/com/wilson/pma/logging/ApplicationLoggerAspect.java
@@ -1,7 +1,13 @@
 package com.wilson.pma.logging;
 
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,12 +20,14 @@ public class ApplicationLoggerAspect {
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	// package com.wilson.pma.controllers will be affected by this 
-	@Pointcut("within(com.wilson.pma.controllers..*)"
-			+ " || within(com.wilson.pma.dao..*)")
+	@Pointcut("within(com.wilson.pma.controllers..*)")
+			//+ " || within(com.wilson.pma.dao..*)") // non-aktif utk meng-test join point
 	public void definePackagePointcut() {
 		// empty method just to name the location specified in the pointcut
+		// method ini hanya utk meng-include kan package mana yg akan ter-affected
 	}
 	
+	/*
 	//@Before("definePackagePointcut()")
 	@After("definePackagePointcut()") // kita define method yg meng-annotation @Pointcut. Method ini akan berjalan  di setiap baris package wilson.pma.controllers package juga wilson.pma.dao 
 	public void log() {
@@ -30,6 +38,65 @@ public class ApplicationLoggerAspect {
 		// ketika menjalankan salah satu method pada browser while the app running,
 		// misal buka display employee, maka log di atas akan muncul pada console
 		// @After() dan @Before() ini bisa juga disebut advice dalam AOP (Aspect Oriented Programming) land 
+	}
+	*/
+	
+	// Join Point -> A Join Point is basically a point during the execution of your program
+	// @Before("definePackagePointcut()")
+	//@After("definePackagePointcut()")
+	//public void logBefore(JoinPoint jp) {
+	@Around("definePackagePointcut()") // Advice @Around seperti gabungan dari kedua advice @After @Before
+	public Object logAround(ProceedingJoinPoint jp) {
+//		log.debug("\n \n \n");
+//		log.debug("************* Before Method Execution *************** \n {}...{} () with arguments[s] = {}",
+//				jp.getSignature().getDeclaringTypeName(),
+//				jp.getSignature().getName(), Arrays.toString(jp.getArgs()));
+//		log.debug("_________________________________________________ \n \n \n ");
+		
+		// sample log with advice Before
+		//2022-05-22 00:47:27.756[0;39m [32mDEBUG[0;39m [35m13072[0;39m [2m---[0;39m [2m[nio-8080-exec-2][0;39m [36mc.w.pma.logging.ApplicationLoggerAspect [0;39m [2m:[0;39m ************* Before Method Execution *************** 
+		// com.wilson.pma.controllers.HomeController...displayHome () with arguments[s] = [{}]
+		//[2m2022-05-22 00:47:27.757[0;39m [32mDEBUG[0;39m [35m13072[0;39m [2m---[0;39m [2m[nio-8080-exec-2][0;39m [36mc.w.pma.logging.ApplicationLoggerAspect [0;39m [2m:[0;39m _________________________________________________ 
+		 
+		// sample log with advice After
+		//[2m2022-05-22 00:50:24.617[0;39m [32mDEBUG[0;39m [35m13072[0;39m [2m---[0;39m [2m[nio-8080-exec-1][0;39m [36mc.w.pma.logging.ApplicationLoggerAspect [0;39m [2m:[0;39m ************* Before Method Execution *************** 
+		// com.wilson.pma.controllers.HomeController...displayHome () with arguments[s] = [{versionNumber=dev, projectsList=[], projectStatusCnt=[], employeesListProjectsCnt=[]}]
+		//[2m2022-05-22 00:50:24.617[0;39m [32mDEBUG[0;39m [35m13072[0;39m [2m---[0;39m [2m[nio-8080-exec-1][0;39m [36mc.w.pma.logging.ApplicationLoggerAspect [0;39m [2m:[0;39m _________________________________________________ 
+		 
+		
+		log.debug("\n \n \n");
+		log.debug("************* Before Method Execution *************** \n {}...{} () with arguments[s] = {}",
+				jp.getSignature().getDeclaringTypeName(),
+				jp.getSignature().getName(), Arrays.toString(jp.getArgs()));
+		log.debug("_________________________________________________ \n \n \n ");
+		
+		log.debug("\n \n \n");
+		log.debug("************* Before Method Execution *************** \n {}...{} () with arguments[s] = {}",
+				jp.getSignature().getDeclaringTypeName(),
+				jp.getSignature().getName(), Arrays.toString(jp.getArgs()));
+		log.debug("_________________________________________________ \n \n \n ");
+		
+		Object o = null;
+		
+		try {
+			o = jp.proceed();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		log.debug("************* After Method Execution *************** \n {}...{} () with arguments[s] = {}",
+				jp.getSignature().getDeclaringTypeName(),
+				jp.getSignature().getName(), Arrays.toString(jp.getArgs()));
+		log.debug("_________________________________________________ \n \n \n ");
+		
+		log.debug("\n \n \n");
+		log.debug("************* Before Method Execution *************** \n {}...{} () with arguments[s] = {}",
+				jp.getSignature().getDeclaringTypeName(),
+				jp.getSignature().getName(), Arrays.toString(jp.getArgs()));
+		log.debug("_________________________________________________ \n \n \n ");
+		
+		return o;
 	}
 	
 }
