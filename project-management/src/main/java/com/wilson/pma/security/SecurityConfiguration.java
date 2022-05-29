a1bd48112dedb7f4601553e43b0a0ecf062ff545
Joe Wilson <wilsonmangunsong@gmail.com>
2022-05-29 13:45:59 +0700

    sec12_vid55

--- project-management/src/main/java/com/wilson/pma/security/SecurityConfiguration.java
+++ project-management/src/main/java/com/wilson/pma/security/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -18,6 +19,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	DataSource dataSource;
+	
+	@Autowired
+	BCryptPasswordEncoder bCryptEncoder; // ingatkan, kita menginject class WebConfig (dimana dia sudah menggunakan annotatino @@Configuration dan ada @Bean dgn type BCryptPasswordEncoder)
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -58,6 +62,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 					.roles("ADMIN");
 		*/
 		
+		/*
 		// Using JDBC with customize schema using query
 		auth.jdbcAuthentication().dataSource(dataSource)
 			.usersByUsernameQuery("select username, password, enabled "
@@ -70,14 +75,27 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		// on jdbc h2 http://localhost:8080/h2-console
 		// also you can see an AUTHORITIES table that contains the authority of the users
 		// the documentation >> https://docs.spring.io/spring-security/site/docs/3.0.x/reference/appendix-schema.html
+		*/
+	
+		// using a real database
+		auth.jdbcAuthentication()
+			.usersByUsernameQuery("select username, password, enabled "
+					+ "from user_accounts where username = ?" ) // look up for the username
+			.authoritiesByUsernameQuery("select username, role "
+					+ "from user_accounts where username = ?")
+			.dataSource(dataSource)
+			.passwordEncoder(bCryptEncoder);
+		
 	}
 	
+	/*
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance(); // ada garis karena deprecreated, ktia gunakan ini hanya utk melihat functionality dari 
 		// configure kita di atas, agar kita bisa pake password yg kita defina di atas "password"
 		// istilahnya meng-inject
 	}
+	*/
 	
 	// this method will give the authorization to take activity over our websites,
 	// only an authorized user can do the task
@@ -85,16 +103,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception{
 		http.authorizeHttpRequests()
 			.antMatchers("/projects/new").hasRole("ADMIN") // only user that has role ADMIN could create project
+			.antMatchers("/projects/save").hasRole("ADMIN")
 			.antMatchers("/employees/new").hasRole("ADMIN")
+			.antMatchers("/employees/save").hasRole("ADMIN")
 			// using this method if using jdbc
-			.antMatchers("/h2_console/**").permitAll()
-			.antMatchers("/").authenticated().and().formLogin(); // all users can access to the endpoint "/" if they're authenticated
-		// ketika user yg tidak mempunyai ROLE ADMIN mencoba akses ke endpoint "/project/new"
+			//.antMatchers("/h2_console/**").permitAll()
+//			.antMatchers("/").authenticated().and().formLogin(); // all users can access to the endpoint "/" if they're authenticated
+			.antMatchers("/", "/**").permitAll() // urutan ini berpengaruh pada akses aplikasi, jika kita menaruh baris ini paling atas, karena di baris ini kita menggunakan "/**" yakni semua url/akses, maka rule berikutnya jadi tidak berguna
+			.and()
+			.formLogin();
+			// ketika user yg tidak mempunyai ROLE ADMIN mencoba akses ke endpoint "/project/new"
 		// maka aplikasi akan menolak
 		
 		// disable this if using jdbc
-		http.csrf().disable();
-		http.headers().frameOptions().disable();
+//		http.csrf().disable();
+//		http.headers().frameOptions().disable();
 		
 		
 	}
